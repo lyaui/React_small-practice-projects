9b0cae8268aa806fe2968806800a2e663549a035
Iris Huang <lyaui@outloook.com>
2021-03-24 11:28:31 +0800

    Styled components GlobalStyles

--- karl-hadwen_clone-netflix/src/index.js
+++ karl-hadwen_clone-netflix/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import GlobalStyle from './global-style';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyle></GlobalStyle>
+    <App />
+  </>,
+  document.getElementById('root'),
+);
